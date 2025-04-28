4fe8faae6a906bdedc30e509ad7f5f1b087d82fa
Renan Toniolo Rocha <renantoniolo@gmail.com>
2025-04-28 20:47:10 -0300

    Initial Commit

---
+++ app_cadastro/app_cadastroApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_cadastroApp.swift
+//  app_cadastro
+//
+//  Created by Renan Toniolo Rocha on 28/04/25.
+//
+
+import SwiftUI
+
+@main
+struct app_cadastroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
